71c240f99f3403ba116ca232b79f7c07bb305347
gradiuscypher <gradiuscypher@gmail.com>
2020-07-06 22:42:40 -0700

    2020-07-06-core map update

---
+++ docs/maps/core/overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "worlds": [
+        "Core01"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1594099899",
+        "controls": {
+            "pan": true,
+            "mapType": true,
+            "coordsBox": true,
+            "zoom": true,
+            "compass": true,
+            "overlays": true,
+            "spawn": true
+        },
+        "north_direction": "lower-left"
+    },
+    "CONST": {
+        "LOWERRIGHT": 2,
+        "UPPERRIGHT": 1,
+        "UPPERLEFT": 0,
+        "LOWERLEFT": 3,
+        "mapDivId": "mcmap",
+        "tileSize": 384,
+        "image": {
+            "signMarker": "signpost_icon.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "bedMarker": "bed.png",
+            "queryMarker": "markers/marker_location.png",
+            "defaultMarker": "signpost.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "2": "compass_lower-right.png",
+                "3": "compass_lower-left.png"
+            },
+            "queryMarker2x": "markers/marker_location_2x.png"
+        }
+    },
+    "tilesets": [
+        {
+            "maxZoom": 9,
+            "name": "Daytime Render",
+            "bgcolor": "#1a1a1a",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "last_rendertime": 1594099222,
+            "north_direction": 0,
+            "spawn": [
+                80,
+                96,
+                192
+            ],
+            "base": "",
+            "world": "Core01",
+            "imgextension": "png",
+            "defaultZoom": 1,
+            "minZoom": 0,
+            "path": "day",
+            "zoomLevels": 9,
+            "center": [
+                80,
+                96,
+                192
+            ],
+            "showlocationmarker": true
+        }
+    ]
+};
