48ab7d59cc1e22dda8e48e557a7c9e24f1b83b43
gradiuscypher <gradiuscypher@gmail.com>
2020-07-07 00:04:44 -0700

    2020-07-07-rgg map update

---
+++ docs/maps/rgg/overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "CONST": {
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "UPPERLEFT": 0,
+        "mapDivId": "mcmap",
+        "tileSize": 384,
+        "image": {
+            "bedMarker": "bed.png",
+            "defaultMarker": "signpost.png",
+            "queryMarker": "markers/marker_location.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "spawnMarker": "markers/marker_home.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "2": "compass_lower-right.png",
+                "3": "compass_lower-left.png"
+            },
+            "signMarker": "signpost_icon.png"
+        },
+        "LOWERRIGHT": 2
+    },
+    "map": {
+        "debug": true,
+        "north_direction": "lower-left",
+        "cacheTag": "1594105394",
+        "controls": {
+            "pan": true,
+            "mapType": true,
+            "compass": true,
+            "zoom": true,
+            "spawn": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "worlds": [
+        "world"
+    ],
+    "tilesets": [
+        {
+            "bgcolor": "#1a1a1a",
+            "defaultZoom": 1,
+            "isOverlay": false,
+            "zoomLevels": 8,
+            "center": [
+                3,
+                80,
+                12
+            ],
+            "north_direction": 0,
+            "last_rendertime": 1594104980,
+            "showlocationmarker": true,
+            "base": "",
+            "poititle": "Markers",
+            "spawn": [
+                3,
+                80,
+                12
+            ],
+            "imgextension": "png",
+            "path": "day",
+            "name": "Daytime Render",
+            "maxZoom": 8,
+            "world": "world",
+            "minZoom": 0
+        }
+    ]
+};
